8de3c114f4cd6e788e42a13106239926cafda054
zhaoesther <ewzhao@ucsd.edu>
2018-11-01 16:40:40 -0700

    Skeleton started

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$(".button").hover(function(){
+    $(this).css("background-color", "coral");
+    }, function(){
+    $(this).css("background-color", "#a6d31d");
+});
+
+
+
+
+
+});
