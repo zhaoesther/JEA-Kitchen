af31eec90febb171d92b46bd166b917598f10454
zhaoesther <42625445+zhaoesther@users.noreply.github.com>
2018-11-01 22:30:37 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-	$(".button").hover(function(){
-        $(this).css("background-color", "coral");
-    }, function(){
-        $(this).css("background-color", "#a6d31d");
-    });
-
     $('navbar a.nav-item').click(function()
     {
       $('navbar a.nav-item').removeClass(" active");
       $(this).addClass(" active");
+    });
+	
+
+    $("#panel").hide(0);
+    $("#add-ingredient").click(function(){
+    	$("#panel").show(500);
     });
 
 
