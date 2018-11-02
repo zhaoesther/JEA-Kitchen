ac2237e02984b86d59126408699acce3e4c3b4bf
jfong02 <jfong02@users.noreply.github.com>
2018-11-01 22:04:31 -0700

    more navbar changes, also attempt of jquery active

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 	$(".button").hover(function(){
-    $(this).css("background-color", "coral");
+        $(this).css("background-color", "coral");
     }, function(){
-    $(this).css("background-color", "#a6d31d");
-});
+        $(this).css("background-color", "#a6d31d");
+    });
 
-
-
+    $('navbar a.nav-item').click(function()
+    {
+      $('navbar a.nav-item').toggleClass("active");
+    });
 
 
 });
