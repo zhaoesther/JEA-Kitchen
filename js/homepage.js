ecfd86cb2ca786c280f03ff42560b470f012c3bf
zhaoesther <42625445+zhaoesther@users.noreply.github.com>
2018-11-01 22:33:50 -0700

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -4,12 +4,6 @@ $(document).ready(function() {
       $('navbar a.nav-item').removeClass(" active");
       $(this).addClass(" active");
     });
-	
-
-    $("#panel").hide(0);
-    $("#add-ingredient").click(function(){
-    	$("#panel").show(500);
-    });
 
 
 });
