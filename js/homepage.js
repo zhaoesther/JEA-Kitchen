b8ba3ec788ce3a43d1463dc04c7c2651ae0ff02c
jfong02 <jfong02@users.noreply.github.com>
2018-11-07 19:39:44 -0800

    Add item menu, icons on navbar and menu items, URL-based active class
    Also attempted at looking at URL to make active class on navbar

--- js/homepage.js
+++ js/homepage.js
@@ -2,12 +2,10 @@ $(document).ready(function() {
     
     
     // Navbar Active Selection
-    $('.nav-item').click(function()
-    {
-      $('navbar a.nav-item').find(".active").removeClass(" active");
-      $(this).parent().addClass(" active");
-      setNavigation();
-      console.log("Figure out how to change active class")
-    });
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+          return this.href == url;
+    }).parent().addClass('active');
 
 });
--- js/kitchen.js
+++ js/kitchen.js
@@ -13,4 +13,44 @@ $(document).ready( function() {
       console.log("Figure out how to change active class");
     });
     
+    // Get the modal
+    var kitchenmodal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+        kitchenmodal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        kitchenmodal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == kitchenmodal) {
+            kitchenmodal.style.display = "none";
+        }
+    }
+
+    // SEARCH FOR INGREDIENT SEARCHBAR INPUTS
+    // Get the input field
+    var input = document.getElementById("myInput");
+
+    // Execute a function when the user releases a key on the keyboard
+    input.addEventListener("keyup", function(event) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.keyCode === 13) {
+        // Trigger the button element with a click
+        document.getElementById("submit").click();
+      }
+    });
 });
